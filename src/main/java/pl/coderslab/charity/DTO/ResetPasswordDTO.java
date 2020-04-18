969e1ae2b833b37235c92a5983f42de2ee5d2177
obol007 <obol007@gmail.com>
2020-04-18 23:01:38 +0200

    removed email constraints

--- src/main/java/pl/coderslab/charity/DTO/ResetPasswordDTO.java
+++ src/main/java/pl/coderslab/charity/DTO/ResetPasswordDTO.java
@@ -3,13 +3,14 @@ package pl.coderslab.charity.DTO;
 import lombok.Data;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
 @Data
 public class ResetPasswordDTO {
 
     Long id;
-    @Email(message = "Podaj prawidłowy e-mail")
+    @NotBlank(message = "Podaj e-mail")
     private String email;
 //    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", message = "Hasło musi posiadać co najmniej 8 znaków" +
 //            " w tym co najmniej jedną wielką literę, małą literę, liczbę i znak specjalny")
